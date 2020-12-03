86c96f509c0156adb8ae09e2c4e52470a887008b
hehao <antoniovance123@gmail.com>
2020-12-03 19:10:49 +0800

    fix

---
+++ blanco/models/inventory.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+
+from blanco.database import Base
+
+
+class Inventory(Base):
+    id = Column(Integer, primary_key=True)
+    location_id = Column(Integer, ForeignKey('location.id'))
+    product_id = Column(Integer, ForeignKey('product_id'))
+    stock = Column(Float)
+    unit_id = Column(Integer, ForeignKey('unit.id'))
+
+    def __init__(self, location_id, product_id, stock, unit_id):
+        self.location_id = location_id
+        self.product_id = product_id
+        self.stock = stock
+        self.unit_id = unit_id
+    
+    def __repr__(self):
+        return "<inventory {}>".format(self.id)
+        
+    
---
+++ blanco/models/sku.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy import Float
+
+from blanco.database import Base
+
+
+class Sku(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120))
+    product_id = Column(Integer, ForeignKey("product.id"))
+    color = Column(String(32))
+    size = Column(String(32))
+    stock = Column(Float)
+    unit_id = Column(Integer, ForeignKey("unit.id"))
+
+    def __init__(self, name, product_id, color, size, stock, unit_id):
+        self.name = name
+        self.product_id = product_id
+        self.color = color
+        self.size = size
+        self.stock = stock
+        self.unit_id = unit_id
+    
+
+    def __repr__(self):
+        return "<sku {}>".format(self.name)
+        
